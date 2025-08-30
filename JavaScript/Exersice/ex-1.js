13d473e5e5cb6d2973d7ea2e2f5998e3de2224f4
Borno <ornob.2202@gmail.com>
2025-08-30 14:34:44 +0600

    exercise added

---
+++ JavaScript/Exersice/ex-1.js
@@ -0,0 +1,30 @@
+//1a
+alert('Good morning');
+
+//1b
+alert('borno-22');
+
+//1c
+10+5
+
+//1d
+20-5
+
+//1e
+2+2-5
+
+//1f
+doucument.body.innerHTML='Good morning!';
+
+//1g
+document.body.innerHTML = 'borno-22';
+
+//1h
+10+8+20
+
+//1i
+100-20-50+200
+
+//lj
+document.body.innerHTML='';
+
---
+++ JavaScript/Exersice/ex-3.js
@@ -0,0 +1,17 @@
+//3a
+'My name is:'
+
+//3b
+'borno'
+
+//3c
+'My name is:'+'borno'
+
+//3d
+'Total cost: $'+(5+3)
+
+//3e
+`Total cost: $${5+3}`
+
+//3f
+alert(`Total cost: $${5+3}`)
