1703d1608e28b5567163d6dfdbc8f756fbca7e3f
Borno <ornob.2202@gmail.com>
2025-08-30 20:36:13 +0600

    completed exercise 3

--- JavaScript/Exersice/ex-3.js
+++ JavaScript/Exersice/ex-3.js
@@ -15,3 +15,27 @@
 
 //3f
 alert(`Total cost: $${5+3}`)
+
+//3g
+'Total cost: $'+(599+295)/100
+
+//3h
+`Total cost: $${(599+295)/100}`
+
+//3i
+alert(`Total cost: $${(599+295)/100}`)
+
+//3j
+alert(`Total cost: $${(599+295)/100} \nThank you, come again!`)
+
+//3k
+`Items (${2+2}): $${(2095*2+799*2)/100}`
+
+//3l
+`Shipping & handling: ${(499+499)/100}`
+
+//3m
+`Total before tax: $${(2095*2+799*2+499+499)/100}`
+
+//3n
+`Estimated tax (10%): $${Math.round((2095*2+799*2+499+499)*0.1)/100}`
